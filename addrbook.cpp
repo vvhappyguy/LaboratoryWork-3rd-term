746a17d9bcee0d6d8eec37760e0dee827e6f9863
vvhappyguy <vvhappyguy@gmail.com>
2018-11-04 00:37:13 +0300

    Add singleton DB, parsing command with >/dev/null. First reads and writes to binary file.

--- addrbook.cpp
+++ addrbook.cpp
@@ -2,10 +2,16 @@
 #include <cstddef>
 #include <algorithm>
 #include <cstring>
+#include <fstream>
 
+#define FName "addbook1231.bin"
+
+using namespace std;
+
+#pragma pack(push, 1)
 class Rec
 {
-	public:
+  public:
 	char LastName[50];
 	char FirstName[50];
 	char FatherName[50];
@@ -13,15 +19,19 @@ class Rec
 	char Address[100];
 	char PhoneNumber[20];
 	char Note[4096];
-	char* field(const char* Id);
+	char *field(const char *Id);
 };
+#pragma pack(pop)
 
 struct FieldMap
 {
-	char   Id[20];
+	char Id[20];
 	size_t offset;
-} fields [] = {
-#define RegField(FIELD) { #FIELD , offsetof(Rec,  FIELD)}
+} fields[] = {
+#define RegField(FIELD)              \
+	{                                \
+#FIELD, offsetof(Rec, FIELD) \
+	}
 	RegField(LastName),
 	RegField(FirstName),
 	RegField(FatherName),
@@ -30,46 +40,154 @@ struct FieldMap
 	RegField(PhoneNumber),
 	RegField(Note)
 #if 0	
-	{"F", offsetof(Rec, F)},
-	{"I", offsetof(Rec, I)},
-	{"O", offsetof(Rec, O)}
-#endif	
+		{"LastName", offsetof(Rec, LastName)},
+		{"FirstName", offsetof(Rec, FirstName)},
+		{"FatherName", offsetof(Rec, FatherName)},
+		{"HB", offsetof(Rec, HB)},
+		{"Address", offsetof(Rec, Address)},
+		{"PhoneNumber", offsetof(Rec, PhoneNumber)},
+		{"Note", offsetof(Rec, Note)}
+#endif
 #undef RegField
 };
-static const size_t FM_size = sizeof(fields)/sizeof(FieldMap);
 
-char* Rec::field(const char* Id)
+static const size_t FM_size = sizeof(fields) / sizeof(FieldMap);
+
+char *Rec::field(const char *Id)
 {
-	FieldMap* fm_ptr = std::find_if(fields, fields+FM_size,
-			[Id](const FieldMap& fm)->bool{return strcmp(Id, fm.Id) == 0;});
+	FieldMap *fm_ptr = std::find_if(fields, fields + FM_size,
+									[Id](const FieldMap &fm) -> bool { return strcmp(Id, fm.Id) == 0; });
 
-  return fm_ptr == fields+FM_size ? 
-		NULL : reinterpret_cast<char*>(this) + fm_ptr->offset;
+	return fm_ptr == fields + FM_size ? NULL : reinterpret_cast<char *>(this) + fm_ptr->offset;
+}
+
+class DB
+{
+  public:
+	static DB &Instance()
+	{
+		static DB theSingleInstance;
+		return theSingleInstance;
+	}
+
+	void connect()
+	{
+		
+		if (!fstream(FName).good())
+		{
+			cout << "Do you want to create new DB by name: " << FName << " (yes/no)" << std::endl;
+			char *ans = new char[3];
+			cin >> ans;
+			if (strcmp(ans,"yes"))
+			{
+				delete ans;
+				throw runtime_error("Bad filename of DB");
+			}
+
+			else
+			{
+				delete ans;
+				ofstream ofs(FName); 
+				ofs.close();
+			}
+		}
+		fstream fileDB(FName);
+		size_t size = 0;
+		fileDB.seekg(0, std::ios::end);
+		size = fileDB.tellg();
+		fileDB.close();
+		if (size % sizeof(Rec) != 0)
+		{
+			throw runtime_error("Bad structure of DB file");
+		}
+	}
+
+	ofstream* getOF()
+	{
+		if(!_ofs)
+			_ofs = new ofstream(FName);
+		return _ofs;
+	}
+
+	ifstream* getIF()
+	{
+		if(_ifs)
+			_ifs = new ifstream(FName);
+		return _ifs;
+	}
+
+	void closeOF()
+	{
+		delete _ofs;
+	}
+
+	void closeIF()
+	{
+		delete _ifs;
+	}
+
+  private:
+	ofstream* _ofs;
+	ifstream* _ifs;
+	DB(){};
+	DB(const DB &root) = delete;
+	DB &operator=(const DB &) = delete;
+};
+
+void parse_command(const char *command)
+{
+	if (strlen(command) > 0)
+	{
+		if (strcmp(command, "add") == 0)
+		{
+			cout << "Add /dev/null\n";
+			return;
+		}
+		if (strcmp(command, "edit") == 0)
+		{
+			cout << "Edit /dev/null\n";
+			return;
+		}
+		if (strcmp(command, "del") == 0)
+		{
+			cout << "Del /dev/null\n";
+			return;
+		}
+		if (strcmp(command, "find") == 0)
+		{
+			cout << "Find /dev/null\n";
+			return;
+		}
+		if (strcmp(command, "list") == 0)
+		{
+			cout << "List /dev/null\n";
+			return;
+		}
+		if (strcmp(command, "count") == 0)
+		{
+			cout << "Count /dev/null\n";
+			return;
+		}
+	}
+	cout << "Please read list of commands:" << std::endl
+		 << "\tadd <...> \n\t- for adding new Rec to DB. \n\t\t(example: add Dyakin Ivan Pavlovich 09.09.1999 Moscow @riokin www.github.com/vvhappyguy)\n"
+		 << "\tedit <n> <fieldname> <val> \n\t- for editting <fieldName> field of <n> Rec from db by your value <val> \n\t\t(example: edit 1 Address SergievPosad)\n"
+		 << "\tdel <n> \n\t- for deletting <n> Rec from DB \n\t\t(example: del 2)\n";
+	return;
 }
 
 int main()
 {
+	DB::Instance().connect();
+	DB::Instance().getIF();
 
-	Rec rec = {"Ivanov", "Ivan", "Ivanych","e","e","e","e"};
-
-	char Id[20];
-
-	while(true)
+	char command[50] = "";
+	for (;;)
 	{
-		std::cout << "Id:" << std::flush; std::cin >> Id;
-		char* field = rec.field(Id);
-		if(field)		
-			std::cout << "rec." << Id << " = " << field << std::endl;
-		else
-			std::cout << "No such field" << std::endl;
+		cout << "> ";
+		cin >> command;
+		parse_command(command);
 	}
 
 	return 0;
 }
-
-
-
-
-
-
-
